66d6f2ec7061728135af65175de5658931e43e3c
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-23 18:12:12 +0100

    header downer done

--- src/components/HeaderDowner.vue
+++ src/components/HeaderDowner.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="header-downer">
-
+    <div id="content-header">
+        <h6>
+            Content goes here 
+        </h6>
+    </div>
   </div>
 </template>
 
@@ -17,8 +21,22 @@ export default {
 <style scoped lang="scss">
 
 #header-downer{
-    height: 50%;
     background-color: #1C1C1C;
+    color: white;
+    // height: 50%;
+   
+        #content-header{
+            padding: 2% 0;
+            width: 80%;
+            margin: auto;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+
+                h6{
+                    margin-left: 2%;
+                }
+        }
 }
 
 </style>
