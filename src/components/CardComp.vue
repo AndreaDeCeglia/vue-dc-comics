7b278c2d8b75807c4cbd38a39f6db54bf8d3ecb6
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-24 16:06:55 +0100

    cards done

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,17 +1,30 @@
 <template>
-  <div id="card"></div>
+  <div id="card">
+    <img :src="comicsDetails.thumb" alt="">
+    <h6> {{ comicsDetails.series }} </h6>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CardComp',
   props: {
-    msg: String
+    comicsDetails: Object,
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+#card{
+  width: calc(100% / 6);
+  padding: 1%;
+
+    img{
+      width: 100%;
+      aspect-ratio: 1/1;
+    }
+}
 
 </style>
